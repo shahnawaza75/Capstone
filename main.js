8d652837dc85b02ee906dee298394cb1f98d78e0
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-04-22 15:52:19 +0530

    Fixed linters

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
 function openNav() {
-    document.getElementById('myNav').style.display = 'block';
-  }
-  
-  function closeNav() {
+  document.getElementById('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.display = 'none';
+}
+
+document.querySelector('.hamburger-span').addEventListener('click', openNav);
+document.querySelector('.closebtn').addEventListener('click', closeNav);
+document.querySelectorAll('.web-nav-link').forEach((link) => {
+  link.addEventListener('click', () => {
     document.getElementById('myNav').style.display = 'none';
-  }
-  
-  document.querySelector('.hamburger-span').addEventListener('click', openNav);
-  document.querySelector('.closebtn').addEventListener('click', closeNav);
-  document.querySelectorAll('.web-nav-link').forEach((link) => {
-    link.addEventListener('click', () => {
-      document.getElementById('myNav').style.display = 'none';
-    });
   });
-  
-  const devSections = document.querySelector('.dev_section');
-  
-  const devList = [
-    {
-      name: 'Emmanuel Jolugbo',
-      description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
-      imageLink: 'images/temi.png',
-      nickName: ['Code Specialist'],
-    },
-    {
-      name: 'Toyosi Taiwo',
-      description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
-      imageLink: 'images/toyosi.png',
-      nickName: ['Yorsy_Dev'],
-    },
-    {
-      name: 'Shaquille Ndunda',
-      description: 'A Professional softawre developer all the way from Kenya. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
-      imageLink: 'images/shaq.png',
-      nickName: ['Shaq Dev'],
-    },
-    {
-      name: 'Yussuf Bashir',
-      description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
-      imageLink: 'images/bash.png',
-      nickName: ['Bash'],
-    },
-  ];
-  
-  devSections.innerHTML = '';
-  for (let i = 0; i < devList.length; i += 1) {
-    devSections.innerHTML += `
+});
+
+const devSections = document.querySelector('.dev_section');
+
+const devList = [
+  {
+    name: 'Emmanuel Jolugbo',
+    description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
+    imageLink: 'images/temi.png',
+    nickName: ['Code Specialist'],
+  },
+  {
+    name: 'Toyosi Taiwo',
+    description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
+    imageLink: 'images/toyosi.png',
+    nickName: ['Yorsy_Dev'],
+  },
+  {
+    name: 'Shaquille Ndunda',
+    description: 'A Professional softawre developer all the way from Kenya. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
+    imageLink: 'images/shaq.png',
+    nickName: ['Shaq Dev'],
+  },
+  {
+    name: 'Yussuf Bashir',
+    description: 'A Professional softawre developer all the way from Lagos, Nigeria. He is one of the great devs out there in the world known for his design skill and DOM manipulation',
+    imageLink: 'images/bash.png',
+    nickName: ['Bash'],
+  },
+];
+
+devSections.innerHTML = '';
+for (let i = 0; i < devList.length; i += 1) {
+  devSections.innerHTML += `
         <div class='devs'>
             <div class='devs__image'>
               <img src='${devList[i].imageLink}' alt='dev 1' class='img1' />
@@ -63,4 +63,4 @@ function openNav() {
               </div>
       </div>
     `;
-  }
+}
